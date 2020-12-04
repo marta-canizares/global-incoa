a00d74f698143de17bbbe7d40df3577eddda9bd1
marta <marta.s.canizares@gmail.com>
2020-12-04 13:22:35 +0100

    ok

--- forms/contact.php
+++ forms/contact.php
@@ -1,12 +1,46 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
+if (isset($_POST['submit'])) {
+  //ini_set( 'display_errors', 1 ); # REMOVE // FOR DEBUG
+  //error_reporting( E_ALL ); # REMOVE // FOR DEBUG
+  $from = "marta.s.canizares@gmail.com"; // Email con el dominio del Hosting para evitar SPAM
+  $fromName = "CONSULTA-WEB"; // Nombre que saldrá en el email recibido
+  $to = "marta.s.canizares@gmail.com"; // Dirección donde se enviará el formulario
+  $subject = $_POST['subject']; // Asunto del Formulario
+
+  /* Componemos el mensaje */
+  $fullMessage .= "Nombre: " . $_POST['name'] . "\r\n";
+  $fullMessage .= "E-Mail: " . $_POST['email'] . "\r\n";
+  $fullMessage .= "Asunto: " . $_POST['subject'] . "\r\n";
+  $fullMessage .= "Mensaje: " . $_POST['message'] . "\r\n";
+
+  /* Creamos las cabeceras del Email */
+  $headers = "Organization: RPF WEB\r\n";
+  $headers .= "From: " . $fromName . "<" . $from . ">\r\n";
+  $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+  $headers .= "Return-Path: " . $to . "\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "X-Priority: 3\r\n";
+  $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
+
+  /* Enviamos el Formulario*/
+  if (mail($to, $subject, $fullMessage, $headers)) {
+      echo "<center><h2>El E-Mail se ha enviado correctamente!</h2></center>";
+  } else {
+      echo "<center><h2>Ops ! El E-Mail ha fallado!</h2></center>S";
+  }
+}
+
+
+
+
+
+
+
+///
+/*
+
   $receiving_email_address = 'marta.s.canizares@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -24,18 +58,22 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+*/
+
+
+
 ?>
